4aef1ec7deffee90e6940327ef1206413d792e2c
BK1917liwenke <1970616984@qq.com>
2019-11-13 16:09:36 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -2,22 +2,26 @@ const path = require("path");
 
 module.exports = {
     devServer:{
-        "/api":{
-            target:"https://www.dkhs.com",
-            changeOrigin:true
-        }
+        proxy:{
+            "/api":{
+                target:"https://www.dkhs.com",
+                changeOrigin:true
+            }
+        }   
     },
     configureWebpack:{
-        alias:{
-            "@":path.join(__dirname,"./src"),
-            "@api":path.join(__dirname,"./src/api"),
-            "@assets":path.join(__dirname,"./src/assets"),
-            "@common":path.join(__dirname,"./src/common"),
-            "@components":path.join(__dirname,"./src/components"),
-            "@pages":path.join(__dirname,"./src/pages"),
-            "@router":path.join(__dirname,"./src/router"),
-            "@store":path.join(__dirname,"./src/store"),
-            "@utils":path.join(__dirname,"./src/utils"),
+        resolve:{
+            alias:{
+                "@":path.join(__dirname,"./src"),
+                "@api":path.join(__dirname,"./src/api"),
+                "@assets":path.join(__dirname,"./src/assets"),
+                "@common":path.join(__dirname,"./src/common"),
+                "@components":path.join(__dirname,"./src/components"),
+                "@pages":path.join(__dirname,"./src/pages"),
+                "@router":path.join(__dirname,"./src/router"),
+                "@store":path.join(__dirname,"./src/store"),
+                "@utils":path.join(__dirname,"./src/utils"),
+            }
         }
     }
 }
